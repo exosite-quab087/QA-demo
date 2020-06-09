062ae8812e32d9b630fcade5e725b72815a964cf
quab-087 <quab087@smail.nchu.edu.tw>
2020-06-09 11:17:21 +0800

    Add testing page

---
+++ firestore.js
@@ -0,0 +1,8 @@
+var config = {
+    apiKey: "AIzaSyCbaBZ4gRThNi0GJx_NIYNYMSPzAMBOcGQ",
+    authDomain: "listener-8e2cd.firebaseapp.com",
+    projectId: "listener-8e2cd"
+};
+
+firebase.initializeApp(config);
+var db = firebase.firestore();
